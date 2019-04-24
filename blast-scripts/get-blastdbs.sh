94c89bfeefba3d25408b8281ee025fa5b9d08844
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-04-24 08:19:05 -0400

    Workaround missing BLASTDB

--- blast-scripts/get-blastdbs.sh
+++ blast-scripts/get-blastdbs.sh
@@ -6,9 +6,17 @@
 
 export PATH=/bin:/usr/bin
 set -euo pipefail
-shopt -s nullglob
 
 sudo mkdir -m 755 -p /blast/db/
 cd /blast/db/
-sudo update_blastdb.pl --source gcp --decompress ref_viroids_rep_genomes_v5 
-sudo update_blastdb.pl --source ncbi --decompress nt cdd
+#sudo update_blastdb.pl --source gcp --decompress nt_v5
+#sudo update_blastdb.pl --source gcp --decompress ref_viroids_rep_genomes_v5 
+#sudo update_blastdb.pl --source gcp --decompress split-cdd 
+#sudo update_blastdb.pl --source gcp --decompress ref_viruses_rep_genomes_v5
+#sudo update_blastdb.pl --source ncbi --decompress nt
+
+# FIXME: cannot find VIV?
+#sudo update_blastdb.pl --source gcp --decompress NCBI_VIV_nucleotide_sequences_v5
+
+# Temporary workaround (WB-3554)
+sudo gsutil -qm cp gs://blast-db/2019-04-20-05-01-28/genomic/Viruses/NCBI_VIV* .
