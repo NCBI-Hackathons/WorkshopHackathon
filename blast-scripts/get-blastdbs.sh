35b475f26fbed85fc7a7df33d2d8fb9bc4008ca4
carlthewebmaster <leubsdor@ncbi.nlm.nih.gov>
2019-04-24 20:21:07 +0000

    updated blast db and notebook

--- blast-scripts/get-blastdbs.sh
+++ blast-scripts/get-blastdbs.sh
@@ -7,8 +7,8 @@
 export PATH=/bin:/usr/bin
 set -euo pipefail
 
-sudo mkdir -m 755 -p /blast/db/
-cd /blast/db/
+sudo mkdir -m 755 -p /srv/data/shared_data/blast/db/
+cd /srv/data/shared_data/blast/db/
 sudo update_blastdb.pl --source gcp --decompress nt_v5
 sudo update_blastdb.pl --source gcp --decompress ref_viroids_rep_genomes_v5 
 sudo update_blastdb.pl --source gcp --decompress split-cdd 
@@ -19,9 +19,4 @@ sudo update_blastdb.pl --source ncbi --decompress nt
 #sudo update_blastdb.pl --source gcp --decompress NCBI_VIV_nucleotide_sequences_v5
 
 # Temporary workaround (WB-3554)
-#sudo gsutil -qm cp gs://blast-db/2019-04-20-05-01-28/genomic/Viruses/NCBI_VIV* .
-#sudo update_blastdb.pl --source ncbi --decompress other_genomic
-#sudo parallel --joblog /tmp/download-other-genomic.log --eta -t "curl -Os ftp://ftp.ncbi.nlm.nih.gov/blast/db/other_genomic.{}.tar.gz.md5" ::: $(seq -w 0 10) $(seq 11 186)
-# this line doesn't work
-#sudo parallel --joblog /tmp/download-other-genomic.log --eta -t "curl -s ftp://ftp.ncbi.nlm.nih.gov/blast/db/other_genomic.{}.tar.gz | tar -xzf - " ::: $(seq -w 0 10) $(seq 11 186)
-#sudo parallel --joblog /tmp/download-other-genomic.log --eta -t "curl -Os ftp://ftp.ncbi.nlm.nih.gov/blast/db/other_genomic.{}.tar.gz" ::: $(seq -w 0 10) $(seq 11 186)
+sudo gsutil -qm cp gs://blast-db/2019-04-20-05-01-28/genomic/Viruses/NCBI_VIV* .
