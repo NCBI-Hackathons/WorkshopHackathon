6d4d5c99b2fe6e21858c454b7ed4621f9a9a6fd6
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2019-04-24 12:07:55 -0400

    Minor

--- blast-scripts/setup-jupyter-on-gce.sh
+++ blast-scripts/setup-jupyter-on-gce.sh
@@ -7,10 +7,10 @@
 export PATH=/bin:/usr/bin
 set -euo pipefail
 shopt -s nullglob
-
+echo "Do this only once!"
 sudo apt-get install -y python3-pip
 sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install jupyter
 sudo pip install jupyter_http_over_ws
 jupyter serverextension enable --py jupyter_http_over_ws
-sudo pip install pandas
+sudo pip install pandas # Bonus
